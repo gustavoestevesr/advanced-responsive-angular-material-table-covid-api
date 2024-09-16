5ee76927d9fcbe3b8d408c6402e054ad6bcecb77
Gustavo Esteves <gustavoesteves@directon.com.br>
2024-09-16 16:27:29 -0300

    feat: definição de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/covid-dashboard/covid-dashboard.component').then(c => c.CovidDashboardComponent),
+  },
+  {
+    path: 'covid-reports',
+    loadComponent: () => import('./pages/covid-list/covid-list.component').then(c => c.CovidListComponent),
+  },
+  {
+    path: 'covid-report-form',
+    loadComponent: () => import('./dialogs/covid-form-dialog/covid-form-dialog.component').then(c => c.CovidFormDialogComponent),
+  },
+  { path: '',   redirectTo: 'dashboard', pathMatch: 'full' }, // redirect to `first-component`
+  { path: '**',  loadComponent: () => import('./pages/page-not-found/page-not-found.component').then(c => c.PageNotFoundComponent), },  // Wildcard route for a 404 page
+];
