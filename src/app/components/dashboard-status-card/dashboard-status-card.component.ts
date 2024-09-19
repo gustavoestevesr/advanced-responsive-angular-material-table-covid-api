c1825297cf713f6d84a9d52b867b0fe71257656e
Gustavo Esteves <gustavoesteves@directon.com.br>
2024-09-19 15:59:26 -0300

    feat: componente reutilizável para status do dashboard

--- src/app/components/dashboard-status-card/dashboard-status-card.component.ts
+++ src/app/components/dashboard-status-card/dashboard-status-card.component.ts
@@ -3,18 +3,17 @@ import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-status-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule, RouterModule],
   templateUrl: './dashboard-status-card.component.html',
   styleUrl: './dashboard-status-card.component.scss'
 })
 export class DashboardStatusCardComponent {
-
   @Input() icon!: string;
   @Input() count!: number;
   @Input() label!: string;
-
 }
