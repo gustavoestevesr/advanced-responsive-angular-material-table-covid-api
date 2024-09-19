e27125687c8008251aa37f09df570e66c721d504
Gustavo Esteves <gustavoesteves@directon.com.br>
2024-09-19 16:01:08 -0300

    feat: serviço para utilizar o componente de alerta de modo escalável

--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Observable, take } from 'rxjs';
+import { AlertDialogComponent } from '../dialogs/alert-dialog/alert-dialog.component';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AlertService {
+  dialogRef!: MatDialogRef<AlertDialogComponent>;
 
-  constructor() { }
+  constructor(private dialog: MatDialog) {}
+
+  public open(options: any) {
+    this.dialogRef = this.dialog.open(AlertDialogComponent, {
+      data: {
+        title: options.title,
+        message: options.message,
+        cancelText: options.cancelText,
+        confirmText: options.confirmText,
+      },
+      panelClass: options.panelClass,
+    });
+  }
+
+  public confirmed(): Observable<any> {
+    return this.dialogRef.afterClosed().pipe(
+      take(1)
+    );
+  }
+
 }
