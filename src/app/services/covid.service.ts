2c36471a90dbccc0f8e9e7fdbd78022ed9713972
Gustavo Esteves <gustavoesteves@directon.com.br>
2024-09-19 16:00:34 -0300

    feat: serviço covid com cache e busca por API

--- src/app/services/covid.service.ts
+++ src/app/services/covid.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { CountryReport } from '../models/country-report.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CovidService {
+  private covidReportsCache$ = new BehaviorSubject<CountryReport[]>([]);
+  private readonly API = 'https://disease.sh/v3/covid-19';
+  private http = inject(HttpClient);
 
-  constructor() { }
+  public getAll(): Observable<CountryReport[]> {
+    // Check if the cache is empty
+    if (this.covidReportsCache$.getValue().length === 0) {
+      // Fetch data from API and cache it
+      return this.http
+        .get<CountryReport[]>(`${this.API}/countries`)
+        .pipe(tap((res) => this.covidReportsCache$.next(res)));
+    } else {
+      // Return cached data as an Observable
+      return this.covidReportsCache$.asObservable();
+    }
+  }
+
+  public getOneByCountry(country: string): Observable<CountryReport> {
+    return this.http.get<CountryReport>(`${this.API}/countries/${country}`);
+  }
 }
