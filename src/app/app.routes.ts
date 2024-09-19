43170d7b77d36545aa8cac35d67cb54ebb7a5fa5
Gustavo Esteves <gustavoesteves@directon.com.br>
2024-09-19 15:59:40 -0300

    refactor: remoção de rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,10 +9,6 @@ export const routes: Routes = [
     path: 'covid-reports',
     loadComponent: () => import('./pages/covid-list/covid-list.component').then(c => c.CovidListComponent),
   },
-  {
-    path: 'covid-report-form',
-    loadComponent: () => import('./dialogs/covid-form-dialog/covid-form-dialog.component').then(c => c.CovidFormDialogComponent),
-  },
   { path: '',   redirectTo: 'dashboard', pathMatch: 'full' }, // redirect to `first-component`
   { path: '**',  loadComponent: () => import('./pages/page-not-found/page-not-found.component').then(c => c.PageNotFoundComponent), },  // Wildcard route for a 404 page
 ];
