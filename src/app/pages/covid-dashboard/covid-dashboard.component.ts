ba2f34c873875fc11278f08dd14297127c752d8e
Gustavo Esteves <gustavoesteves@directon.com.br>
2024-09-19 16:01:41 -0300

    feat: página covid dashboard

--- src/app/pages/covid-dashboard/covid-dashboard.component.ts
+++ src/app/pages/covid-dashboard/covid-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DashboardComponent } from "../../components/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-covid-dashboard',
   standalone: true,
-  imports: [],
+  imports: [DashboardComponent],
   templateUrl: './covid-dashboard.component.html',
   styleUrl: './covid-dashboard.component.scss'
 })
