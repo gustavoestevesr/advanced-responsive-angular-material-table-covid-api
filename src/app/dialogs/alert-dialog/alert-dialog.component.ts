ead0cbe6aaedcc2d81860d6e5c8eadc33334ba56
Gustavo Esteves <gustavoesteves@directon.com.br>
2024-09-19 15:59:58 -0300

    feat: componente de confirmação em dialog

--- src/app/dialogs/alert-dialog/alert-dialog.component.ts
+++ src/app/dialogs/alert-dialog/alert-dialog.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-alert-dialog',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './alert-dialog.component.html',
-  styleUrl: './alert-dialog.component.scss'
+  styleUrl: './alert-dialog.component.scss',
 })
 export class AlertDialogComponent {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {
+    cancelText: string,
+    confirmText: string,
+    message: string,
+    title: string
+  }, private dialogRef: MatDialogRef<AlertDialogComponent>) { }
+
+  public onCancelClick() {
+    this.dialogRef.close(false);
+  }
+
+  public onConfirmClick() {
+    this.dialogRef.close(true);
+  }
 
 }
